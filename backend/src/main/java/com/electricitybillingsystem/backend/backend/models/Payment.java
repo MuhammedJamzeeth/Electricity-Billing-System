8f6e7e8a97f4647521e73f666bf7a020219691f1
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-11-06 13:47:17 +0530

    feed: paymentmodel for the hibernate to create automatically

---
+++ backend/src/main/java/com/electricitybillingsystem/backend/backend/models/Payment.java
@@ -0,0 +1,35 @@
+package com.electricitybillingsystem.backend.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+@Setter
+@Table(name = "Payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "payment_id")
+    private Integer paymentId;
+
+    @ManyToOne
+    @JoinColumn(name = "account_number", nullable = false)
+    private Consumer consumer;
+
+    @Column(name="receipt_number", nullable = false)
+    private String receiptNumber;
+
+    @Column(name="amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name="payment_date", nullable = false)
+    private Date paymentDate;
+}
