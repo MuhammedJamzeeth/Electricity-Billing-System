b0eee2c1c1de886f728c6ed9543d05bf0a708ea2
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-11-07 02:39:00 +0530

    feat: created a view

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -36,9 +36,9 @@ CREATE TABLE IF NOT EXISTS consumer (
     contact_number VARCHAR(15) NOT NULL
     );
 
-
 CREATE INDEX idx_consumer_id ON consumer(id);
 
+-- siyam
 CREATE TABLE IF NOT EXISTS payment (
     payment_id INT AUTO_INCREMENT PRIMARY KEY,
     receipt_number INT UNIQUE NOT NULL,
@@ -47,7 +47,6 @@ CREATE TABLE IF NOT EXISTS payment (
     account_number BIGINT NOT NULL,
     FOREIGN KEY (account_number) REFERENCES consumer(account_no)
 );
-
 
 DELIMITER //
 
@@ -62,6 +61,21 @@ END //
 
 DELIMITER ;
 
+CREATE VIEW payment_consumer_view AS
+SELECT
+    p.payment_id,
+    p.account_number,
+    p.receipt_number,
+    CONCAT(c.first_name, ' ', c.last_name) AS full_name,
+    c.address,
+    p.payment_date,
+    p.amount
+FROM
+    payment p
+        JOIN
+    consumer c ON p.account_number = c.account_no;
+
+-- akthar
 CREATE TABLE IF NOT EXISTS EBill (
    bill_id BIGINT AUTO_INCREMENT PRIMARY KEY,
    account_no BIGINT NOT NULL,
