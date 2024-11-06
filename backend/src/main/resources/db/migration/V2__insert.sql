0e166aa4fa6011097bc1f5400764328b27af8837
Mohamad Siyam <siyam_2020679@fot.ruh.ac.lk>
2024-11-06 13:49:37 +0530

    feed: write insert queries for consumer,ebill and payment

--- backend/src/main/resources/db/migration/V2__insert.sql
+++ backend/src/main/resources/db/migration/V2__insert.sql
@@ -1,5 +1,6 @@
 INSERT INTO admin (username, password) VALUES ('admin', 'admin');
 
+<<<<<<< Updated upstream
 INSERT INTO consumer (id,account_no,first_name, last_name, email, meter_no, address, phase, contact_number)
 VALUES
     (1,4561897562,'Amal', 'Perera', 'amal.perera@example.com', 1000000001, 'No. 10, Main Street, Colombo 07, Sri Lanka', '1-Phase', '+94-71-123-4567'),
@@ -17,3 +18,28 @@ VALUES
     (13,7562479852,'Shan', 'Fonseka', 'shan.fonseka@example.com', 1000000013, 'No. 70, Main Street, Colombo 07, Sri Lanka', '1-Phase', '+94-76-345-6789'),
     (14,1237324567,'Pradeep', 'Wijesinghe', 'pradeep.wijesinghe@example.com', 1000000014, 'No. 75, Main Street, Colombo 07, Sri Lanka', '1-Phase', '+94-76-456-7890'),
     (15,9825761543,'Thilina', 'Kumara', 'thilina.kumara@example.com', 1000000015, 'No. 80, Main Street, Colombo 07, Sri Lanka', '1-Phase', '+94-76-567-8901');
+=======
+# INSERT INTO consumer (account_no, first_name, last_name, email, meter_no, address, phase, contact_number) VALUES
+# (1000000000, 'John', 'Doe', 'john.doe@example.com', 1234567890, '123 Elm St, Springfield', 'Single', '1234567890'),
+# (1000000001, 'Jane', 'Smith', 'jane.smith@example.com', 1234567891, '456 Oak St, Springfield', 'Three-Phase', '0987654321'),
+# (1000000002, 'Alice', 'Johnson', 'alice.johnson@example.com', 1234567892, '789 Pine St, Springfield', 'Single', '1122334455'),
+# (1000000003, 'Bob', 'Brown', 'bob.brown@example.com', 1234567893, '321 Maple St, Springfield', 'Two-Phase', '2233445566'),
+# (1000000004, 'Charlie', 'Davis', 'charlie.davis@example.com', 1234567894, '654 Cedar St, Springfield', 'Single', '3344556677');
+#
+#
+# INSERT INTO payment (receipt_number, amount, payment_date, account_number) VALUES
+#            (10001, 150.75, '2024-01-15', 1000000000),
+#            (10002, 200.50, '2024-02-20', 1000000001),
+#            (10003, 125.00, '2024-03-10', 1000000002),
+#            (10004, 300.00, '2024-04-25', 1000000003),
+#            (10005, 175.25, '2024-05-30', 1000000004);
+#
+# INSERT INTO EBill (account_no, crnt_date, last_date, total_unit, crnt_unit, month_bill, total_bill) VALUES
+#         (1000000000, '2024-01-01', '2024-01-31', 100, 120, 150.75, 150.75),
+#         (1000000001, '2024-02-01', '2024-02-28', 200, 180, 200.50, 200.50),
+#         (1000000002, '2024-03-01', '2024-03-31', 150, 170, 125.00, 125.00),
+#         (1000000003, '2024-04-01', '2024-04-30', 250, 240, 300.00, 300.00),
+#         (1000000004, '2024-05-01', '2024-05-31', 180, 190, 175.25, 175.25);
+
+
+>>>>>>> Stashed changes
